92141c10edc0f1e7bacda4903f4c735f49259717
Drishya Puthanpurakal Manohar <43372807+drishyapmanohar@users.noreply.github.com>
2026-06-18 04:02:42 -0400

    report db added

---
+++ api/data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<ReportJob> ReportJobs => Set<ReportJob>();
+}
