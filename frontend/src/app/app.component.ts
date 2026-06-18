19f04270f834fcc5bf56eccac4c42cfe6719b9ce
Drishya Puthanpurakal Manohar <43372807+drishyapmanohar@users.noreply.github.com>
2026-06-18 04:09:36 -0400

    final ui

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { ReportSignalrService } from './services/report-signalr.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  constructor(private signalr: ReportSignalrService) {}
+
+  ngOnInit() {
+    this.signalr.startConnection();
+  }
 }
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { GenerateReportComponent } from './pages/generate-report/generate-report.component';
+import { ReportsComponent } from './pages/reports/reports.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'generate',
+    pathMatch: 'full'
+  },
+  {
+    path: 'generate',
+    component: GenerateReportComponent
+  },
+  {
+    path: 'reports',
+    component: ReportsComponent
+  }
+];
